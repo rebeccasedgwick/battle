e5cec9b013b0166ca0676b33cb982eff7cb2e97f
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-23 17:37:07 +0100

    Refactoring

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,5 +1,5 @@
 feature 'Attacking' do
-  scenario 'attack Player 2' do
+  scenario 'attack Player 2 reduce HP by 10' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).not_to have_content('Bob: 60HP')
