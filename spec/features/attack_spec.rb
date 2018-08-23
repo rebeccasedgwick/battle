1a0f62ee577d6c8422c6581de61d131aa05c9630
Lucien Najev <luciennajev@hotmail.com>
2018-08-23 16:58:25 +0100

    added hit points functionality

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,6 +2,7 @@ feature 'Attacking' do
   scenario 'attack Player 2' do
     sign_in_and_play
     click_button 'Attack'
-    expect(page).to have_content('Alice attacked Bob')
+    expect(page).not_to have_content('Bob: 60HP')
+    expect(page).to have_content('Bob: 50HP')
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,32 @@ require "player"
 
 describe Player do
   subject(:bob) {Player.new("Bob")}
+  subject(:alice) {Player.new("Alice")}
 
   describe "#name" do
     it "Returns a player's name" do
       expect(bob.name).to eq "Bob"
     end
   end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(bob.hit_points).to eq(described_class::DEFAULT_HIT_POINTS)
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(bob).to receive(:receive_damage)
+      alice.attack(bob)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { alice.receive_damage }.to change { alice.hit_points }.by(-10)
+    end
+  end
+
+
 end
