2e5edb1558541ef685dd104b29879012d2e3fbe0
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-24 11:06:58 +0100

    Adds #attack method on Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,9 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  subject(:player_1) { double :player }
-  subject(:player_2) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
 
   describe '#attack' do
     it 'damages the player' do
@@ -11,4 +11,21 @@ describe Game do
       game.attack(player_2)
     end
   end
+
+  describe '#initialize' do
+    it 'returns the two player names' do
+      expect(game.player_1).to eq player_1
+      expect(game.player_2).to eq player_2
+    end
+  end
 end
+
+
+
+# describe "#initialize" do
+#   it "returns the number, dish, price, and quantity of an item" do
+#     expect(subject.number).to eq "01"
+#     expect(subject.dish).to eq "meat curry"
+#     expect(subject.price).to eq "6.00"
+#     expect(subject.quantity).to eq "2"
+#   end
