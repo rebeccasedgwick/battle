b85c555123f3e04bce966182475b44acc348aa79
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-24 11:41:18 +0100

    Adds test for player 1 viewing their score

--- spec/features/viewing_points_spec.rb
+++ spec/features/viewing_points_spec.rb
@@ -4,4 +4,10 @@ feature 'View Hit Points' do
     sign_in_and_play
     expect(page).to have_content 'Bob has 60 HP'
   end
+
+  scenario "View Player 2's Hit Points" do
+    visit('/')
+    sign_in_and_play
+    expect(page).to have_content 'Alice has 60 HP'
+  end
 end
